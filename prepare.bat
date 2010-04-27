a37a7c334eea39bd9fa4f66d78716f6e6022d1c4
simpledotnet <simpledotnet@d83cf894-ca0d-11de-a70d-3b9dd4243769>
2010-04-27 07:41:49 +0000

    * update prepare.bat

--- prepare.bat
+++ prepare.bat
@@ -14,5 +14,13 @@ call deldir IQToolkit.Data.OracleClient\obj
 call deldir IQToolkit.Data.OracleClient\bin\debug
 call delfile IQToolkit.Data.OracleClient\bin\release\*.pdb
 
+call deldir IQToolkit.Data.SqlClient\obj
+call deldir IQToolkit.Data.SqlClient\bin\debug
+call delfile IQToolkit.Data.SqlClient\bin\release\*.pdb
+
+call deldir Test\obj
+call deldir Test\bin\debug
+call delfile Test\bin\release\*.pdb
+
 echo.
 pause
